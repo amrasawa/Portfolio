e1f93fd80aff2c92c7627d97fcd6837dbb36a302
amrasawa <karmelsalma5@gmail.com>
2024-10-13 14:46:07 +0300

    sidebar interactive

---
+++ main.js
@@ -0,0 +1,8 @@
+function showSideBar(){
+    let burger= document.querySelector(".sidebar");
+    burger.style.display='flex';
+}
+function hideSideBar(){
+    let sideburger=document.querySelector('.sidebar');
+    sideburger.style.display='none';
+}
